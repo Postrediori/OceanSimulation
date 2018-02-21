e6f0aabdee96984f41a902a761185ee52c67788a
Rasim Labibov <postrediori@gmail.com>
2018-02-21 19:28:00 +0200

    Add X11 Xi library requirement for Linux

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -1,6 +1,14 @@
 find_package(OpenGL REQUIRED)
 find_package(Freetype REQUIRED)
 
+if (CMAKE_SYSTEM_NAME STREQUAL Linux)
+  find_package(X11 REQUIRED)
+
+  if (NOT X11_Xi_FOUND)
+    message(FATAL_ERROR "X11 Xi library is required")
+  endif ()
+endif ()
+
 include(cmake/plog.cmake)
 include(cmake/glad.cmake)
 include(cmake/glfw.cmake)
